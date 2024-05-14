a56c00856914d73d917ed12741ac94ebbcec29a5
Andrey Maksimov <andrey@tenderowl.com>
2024-05-14 09:55:15 +0300

    Mark UserId to replace later

--- Norka.Web/Models/Note.cs
+++ Norka.Web/Models/Note.cs
@@ -5,19 +5,18 @@ namespace Norka.Web.Models;
 
 public class Note
 {
-    [MaxLength(32)]
-    public string Id { get; set; } = Nanoid.Generate();
-    [MaxLength(32)]
-    public required string UserId { get; set; }
-    
-    [MaxLength(500)]
-    public string? Title { get; set; }
+    [MaxLength(32)] public string Id { get; set; } = Nanoid.Generate();
+
+    // TODO: Replace with ApplicationUserId
+    [MaxLength(32)] public required string UserId { get; set; }
+
+    [MaxLength(500)] public string? Title { get; set; }
     public required string Content { get; set; }
-    
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? DeletedAt { get; set; }
-    
+
     public bool IsDeleted { get; set; } = false;
     public bool IsArchived { get; set; } = false;
     public bool IsEncrypted { get; set; } = false;
